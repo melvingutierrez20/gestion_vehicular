bf0481eacd9471e93f3283f661bc9200cebc6a6c
melvingutierrez20 <melvin.gutierrez@catolica.edu.sv>
2025-09-12 19:41:36 -0600

    aqui hice mas cambios

--- controladores/buscar_vehiculo.php
+++ controladores/buscar_vehiculo.php
@@ -30,7 +30,12 @@ if (isset($_GET['placa'])) {
         if (count($vehiculo) > 0) {
             // Si el vehículo existe, retorna el vehículo y las esquelas
 
-            //aqui se agerga quipoidgdfgdfg
+            //aqui se agerga una nota para ayuda del usuario
+            //aqui tambien
+
+
+
+
             echo json_encode(['status' => 'success', 'data' => $vehiculo[0]]);
         } else {
             echo json_encode(['status' => 'error', 'message' => 'No se encontró el vehículo con esa placa.']);
